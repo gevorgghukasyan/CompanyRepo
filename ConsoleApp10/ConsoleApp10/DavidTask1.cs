834a1c5afb488e487dbe739c0e6d677129bd2e8a
Dav <dav.dav0@mail.ru>
2025-02-17 12:46:22 +0400

    David Task 1 (Ref)

---
+++ ConsoleApp10/ConsoleApp10/DavidTask1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    internal class DavidTask1
+    {
+        public double RefTask(ref double x, ref double y)
+        {
+            x = Math.Pow(x, 2);
+            y = Math.Pow(y, 2);
+            return x + y;
+        }
+    }
+}
--- ConsoleApp10/ConsoleApp10/Program.cs
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -1,139 +1,150 @@
 ﻿using System.Collections.Generic;
 using System;
+using ConsoleApp10;
 
 namespace opp
 {
-	internal class Program
-	{
-		static void Main(string[] args)
-		{
-			int option = 0;
-			Casino casino = new Casino(100);
-			Bet bet = null;
-			do
-			{
-				Console.WriteLine("Take option");
-				Console.WriteLine("1:NumberBet 2:OddBet 3:EvenBet");
-				option = int.Parse(Console.ReadLine());
-				Console.WriteLine("imput bet amount");
-				int amount = int.Parse(Console.ReadLine());
-				Console.WriteLine("Guess the number");
-				int userNumber = int.Parse(Console.ReadLine());
-				switch (option)
-				{
-					case 0:
-						return;
-					case 1:
-						bet = new NumberBet(userNumber, amount);
-						break;
-					case 2:
-						bet = new OddBet(amount);
-						break;
-					case 3:
-						bet = new EvenBet(amount);
-						break;
-				}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int option = 0;
+            Casino casino = new Casino(100);
+            Bet bet = null;
+            do
+            {
+                Console.WriteLine("Take option");
+                Console.WriteLine("1:NumberBet 2:OddBet 3:EvenBet");
+                option = int.Parse(Console.ReadLine());
+                Console.WriteLine("imput bet amount");
+                int amount = int.Parse(Console.ReadLine());
+                Console.WriteLine("Guess the number");
+                int userNumber = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 0:
+                        return;
+                    case 1:
+                        bet = new NumberBet(userNumber, amount);
+                        break;
+                    case 2:
+                        bet = new OddBet(amount);
+                        break;
+                    case 3:
+                        bet = new EvenBet(amount);
+                        break;
+                }
 
-				casino.PlaceBet(bet);
-			} while (option != 0);
-		}
+                casino.PlaceBet(bet);
+            } while (option != 0);
 
-		class A
-		{
-			private int x;
-			private readonly int y;
+            //-----------David Task 1 ---------------------
 
-			public A()
-			{
-				y = 10;
-			}
+            DavidTask1 task1 = new DavidTask1();
+            double x = 5;
+            double y = 4;
 
-			public void S()
-			{
-				x = 25;
-			}
-		}
+            Console.Write($"{x}^2 + {y}^2 = ");
+            double sum = task1.RefTask(ref x, ref y);
+            Console.WriteLine(sum);
+        }
 
-		class Bet
-		{
-			public int Amount { get; set; }
-			public Bet(int amount)
-			{
-				Amount = amount;
-			}
-			public virtual bool IsWinningBet(int diceRoll)
-			{
-				throw new NotImplementedException();
-			}
-			public virtual int Payout()
-			{
-				return Amount * 2;
-			}
-		}
-		class NumberBet : Bet
-		{
-			public NumberBet(int number, int amount) : base(amount)
-			{
-				Number = number;
-			}
-			public int Number { get; set; }
-			public override bool IsWinningBet(int DiceRole)
-			{
-				return DiceRole == Number;
-			}
-			public override int Payout()
-			{
-				return Amount * 6;
-			}
-		}
-		class OddBet : Bet
-		{
-			public OddBet(int amount) : base(amount)
-			{
-			}
-			public override bool IsWinningBet(int DiceRole)
-			{
-				return DiceRole % 2 != 0;
-			}
-		}
-		class EvenBet : Bet
-		{
-			public EvenBet(int amount) : base(amount)
-			{
-			}
-			public override bool IsWinningBet(int DiceRole)
-			{
-				return DiceRole % 2 == 0;
-			}
-		}
-		class Casino
-		{
-			public Casino(int playerBalance)
-			{
-				PlayerBalance = playerBalance;
-			}
-			public int PlayerBalance { get; set; }
-			public void PlaceBet(Bet bet)
-			{
-				if (PlayerBalance < bet.Amount)
-				{
-					throw new ArgumentException("you havenot money for playing");
-				}
-				PlayerBalance -= bet.Amount;
-				Random random = new Random();
-				int DiceRole = random.Next(1, 7);
-				bool resalt = bet.IsWinningBet(DiceRole);
-				if (resalt)
-				{
-					PlayerBalance += bet.Payout();
-					Console.WriteLine($"you win: your balance is{PlayerBalance}");
-				}
-				else
-				{
-					PlayerBalance -= bet.Payout();
-					Console.WriteLine($"you lose: your balance is{PlayerBalance}");
-				}
-			}
-		}
-	}
+        class A
+        {
+            private int x;
+            private readonly int y;
+
+            public A()
+            {
+                y = 10;
+            }
+
+            public void S()
+            {
+                x = 25;
+            }
+        }
+
+        class Bet
+        {
+            public int Amount { get; set; }
+            public Bet(int amount)
+            {
+                Amount = amount;
+            }
+            public virtual bool IsWinningBet(int diceRoll)
+            {
+                throw new NotImplementedException();
+            }
+            public virtual int Payout()
+            {
+                return Amount * 2;
+            }
+        }
+        class NumberBet : Bet
+        {
+            public NumberBet(int number, int amount) : base(amount)
+            {
+                Number = number;
+            }
+            public int Number { get; set; }
+            public override bool IsWinningBet(int DiceRole)
+            {
+                return DiceRole == Number;
+            }
+            public override int Payout()
+            {
+                return Amount * 6;
+            }
+        }
+        class OddBet : Bet
+        {
+            public OddBet(int amount) : base(amount)
+            {
+            }
+            public override bool IsWinningBet(int DiceRole)
+            {
+                return DiceRole % 2 != 0;
+            }
+        }
+        class EvenBet : Bet
+        {
+            public EvenBet(int amount) : base(amount)
+            {
+            }
+            public override bool IsWinningBet(int DiceRole)
+            {
+                return DiceRole % 2 == 0;
+            }
+        }
+        class Casino
+        {
+            public Casino(int playerBalance)
+            {
+                PlayerBalance = playerBalance;
+            }
+            public int PlayerBalance { get; set; }
+            public void PlaceBet(Bet bet)
+            {
+                if (PlayerBalance < bet.Amount)
+                {
+                    throw new ArgumentException("you havenot money for playing");
+                }
+                PlayerBalance -= bet.Amount;
+                Random random = new Random();
+                int DiceRole = random.Next(1, 7);
+                bool resalt = bet.IsWinningBet(DiceRole);
+                if (resalt)
+                {
+                    PlayerBalance += bet.Payout();
+                    Console.WriteLine($"you win: your balance is{PlayerBalance}");
+                }
+                else
+                {
+                    PlayerBalance -= bet.Payout();
+                    Console.WriteLine($"you lose: your balance is{PlayerBalance}");
+                }
+            }
+        }
+    }
 }
